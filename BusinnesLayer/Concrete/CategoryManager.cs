b2a23382f7684972092adf89dfcf26f01e39ba6d
Azmi Daşman <azmidasman@gmail.com>
2022-05-06 23:39:03 +0300

    Category Manager oluşturuldu.

--- BusinnesLayer/Concrete/CategoryManager.cs
+++ BusinnesLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using System;
@@ -11,33 +12,47 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
-        EFCategoryRepository eFCategoryRepository;
-        public CategoryManager()
+        //EFCategoryRepository eFCategoryRepository; (Bunu Kullanmayacağım!!)
+        //public CategoryManager()
+        //{
+        //    _categoryDal = new EFCategoryRepository();
+        //}
+
+        /*EfCategoryRepository'i kullanmamızdaki dezavantaj Entity Framework'e bağımlı olmamız. 
+         *İlerde başka bir teknoloji geldiğinde projeyi ona geçirmek için neredeyse bütün katmanlardaki kodları tek tek değiştirmemiz 
+         gerekir ama interface kullanarak bu bağımlılığı yok eder ve istersek ileride daha farklı teknolojilere geçebiliriz.
+         *Avantaj olaraksa kısa vadede daha az kod yazıp daha kısa sürede projeyi bitirebiliriz.
+         */
+
+        ICategoryDal _categoryDal; //Field'ı tanımladım.      
+
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            eFCategoryRepository = new EFCategoryRepository();
+            _categoryDal = categoryDal;
         }
+
         public void CategoryAdd(Category category)
         {
-            eFCategoryRepository.Insert(category);
+            _categoryDal.Insert(category);
         }
 
         public void CategoryDelete(Category category)
         {
-            eFCategoryRepository.Delete(category);
+            _categoryDal.Delete(category);
         }
 
         public void CategoryUpdate(Category category)
         {
-            eFCategoryRepository.Update(category);
+            _categoryDal.Update(category);
         }
 
         public Category GetById(int id)
         {
-            return eFCategoryRepository.GetById(id);
+            return _categoryDal.GetById(id);
         }
         public List<Category> GetList()
         {
-            return eFCategoryRepository.GetListAll();
+            return _categoryDal.GetListAll();
         }
 
     }
